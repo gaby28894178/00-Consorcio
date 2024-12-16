f0d2dea2f82ebd78957f426d5e2a04573320a435
GABRIEL GABRIELLI <65731435+gaby28894178@users.noreply.github.com>
2024-12-16 15:13:40 -0300

    Add files via upload
    profe jorge

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('menu-button').addEventListener('click', function() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+});
